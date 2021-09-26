bf71f2cdd79567bf09818662fdb35063dd0ef9cf
GebinGu <89726130+GebinGu@users.noreply.github.com>
2021-09-26 22:56:30 +1000

    Animation

--- Assets/Scripts/PacStuMovement.cs
+++ Assets/Scripts/PacStuMovement.cs
@@ -14,14 +14,6 @@ public class PacStuMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-
-        }
+        
     }
 }
